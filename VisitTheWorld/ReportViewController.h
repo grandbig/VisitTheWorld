1d4905e0b6d2812759aa7ebed22436a7bc9ea8e9
加藤 雄大 <takahiro.kato15@gmail.com>
2015-02-22 13:20:41 +0900

    記録確認用のViewControllerを追加

---
+++ VisitTheWorld/ReportViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReportViewController.h
+//  VisitTheWorld
+//
+//  Created by 加藤 雄大 on 2015/02/22.
+//  Copyright (c) 2015年 grandbig.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReportViewController : UIViewController
+
+@end
---
+++ VisitTheWorld/ReportViewController.m
@@ -0,0 +1,15 @@
+//
+//  ReportViewController.m
+//  VisitTheWorld
+//
+//  Created by 加藤 雄大 on 2015/02/22.
+//  Copyright (c) 2015年 grandbig.github.io. All rights reserved.
+//
+
+#import "ReportViewController.h"
+#import <MapKit/MapKit.h>
+#import <LFHeatMap.h>
+
+@implementation ReportViewController
+
+@end
